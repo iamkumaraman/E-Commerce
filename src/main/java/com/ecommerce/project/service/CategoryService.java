1efb26e6f70c3e86f44cb9994dc768412eabd95f
Amankumar-1999 <amankumarexcellent@gmail.com>
2025-07-04 22:25:06 +0530

    Ecom_001

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    void createCategory(Category category);
+    List<Category> getAllCategories();
+
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
+}
