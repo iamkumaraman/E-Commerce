e53b929c97380413348d428d2627f0552931bd3b
Amankumar-1999 <amankumarexcellent@gmail.com>
2025-07-06 01:06:43 +0530

    Ecom_003

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,15 +1,15 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
 
-import java.util.List;
 
 public interface CategoryService {
 
-    void createCategory(Category category);
-    List<Category> getAllCategories();
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    String deleteCategory(Long categoryId);
+    CategoryDTO deleteCategory(Long categoryId);
 
-    Category updateCategory(Category category, Long categoryId);
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
 }
